5bca17592f4a9db5ee9d0c03ef2fe1407ff41f61
Akshithayadavv <100852271+Akshithayadavv@users.noreply.github.com>
2022-05-13 13:46:40 +0530

    Add files via upload

---
+++ 33-thymeleafdemo-employees-list-css/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Employee.java
@@ -0,0 +1,62 @@
+package com.luv2code.springboot.thymeleafdemo.model;
+
+public class Employee {
+
+    private int id;
+    private String firstName;
+    private String LastName;
+    private String email;
+
+    public Employee(){
+
+    }
+
+    public Employee(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        LastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
